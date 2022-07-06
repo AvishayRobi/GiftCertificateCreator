57b88279e535c60139099ccdfc9e772f9f6d631f
AvishayRobi <79645481+AvishayRobi@users.noreply.github.com>
2022-07-06 10:21:56 +0300

    Update FileManager.cs

--- Files/FileManager.cs
+++ Files/FileManager.cs
@@ -4,12 +4,27 @@ namespace GiftCertificateCreator.Files
 {
   public class FileManager
   {
+    #region Data Members
+    private string fileFullPath { get; }
+    #endregion
+
+    #region Ctor
+    public FileManager(string fullPath)
+    {
+      this.fileFullPath = fullPath;
+    }
+    #endregion
+
     public static string GetTempPath()
       =>
       Path.GetTempPath();
 
-    public void SaveData(string data, string fileFullPath)
+    public void SaveData(string data)
       =>
-      File.WriteAllText(fileFullPath, data);      
+      File.WriteAllText(this.fileFullPath, data);
+
+    public void DeleteSavedFile()
+      =>
+      File.Delete(this.fileFullPath);
   }
 }
